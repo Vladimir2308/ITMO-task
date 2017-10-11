fc081708efa7d26b04cade8e1501ccddacb3d2f6
Vladimir Sushchen <ya_vladimir@list.ru>
2017-10-12 00:02:44 +0300

    1

---
+++ Task4.java
@@ -0,0 +1,12 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int n = 345;
+        int answer = 0;
+        System.out.println("Число n = " + n);
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        System.out.println(" Сумма цифр числа = " + answer);
+    }
+}
