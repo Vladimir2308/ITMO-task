8539a3ea7eeb9721e2f8d27b7cc6214ec6217fbc
Vladimir Sushchen <ya_vladimir@list.ru>
2017-10-16 10:42:32 +0300

    10:42 16.10.2017

---
+++ Lecture2LoopsTask13.java
@@ -0,0 +1,21 @@
+public class Lecture2LoopsTask13 {
+    private static boolean contain2or13(int num) {
+        while (num > 0) {
+            if ((num % 10 == 4) || (num % 100 == 13))
+                return true;
+            num /= 10;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 100000; i++) {
+            if (contain2or13(i)) {
+                count++;
+//                   System.out.println(i + " ");
+            }
+        }
+        System.out.println("Исключить: " + count);
+    }
+}
