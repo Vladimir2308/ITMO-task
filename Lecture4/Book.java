834c4ea8a37cb8ea9e5afcc74296406ce2e5446f
Vladimir Sushchen <ya_vladimir@list.ru>
2017-10-20 18:29:12 +0300

    Лекция 4, Выполнена задачи 4

---
+++ Lecture4/Book.java
@@ -0,0 +1,12 @@
+package Lecture4;
+
+public class Book {
+    String author, title;
+    int pagesNum;
+
+    Book(String author, String title, int pagesNun) {
+        this.author = author;
+        this.title = title;
+        this.pagesNum = pagesNum;
+    }
+}
---
+++ Lecture4/Library.java
@@ -0,0 +1,41 @@
+package Lecture4;
+
+public class Library {
+    Object[][] array = new Object[1000][2];
+    int count = 0;
+
+    void put(Book book, int quantity) {
+        if (arrayIndex(book) < 0) {
+            array[count][0] = book;
+            array[count++][1] = quantity;
+        } else
+            array[arrayIndex(book)][1] = (Integer) array[arrayIndex(book)][1] + quantity;
+    }
+
+    int get(Book book, int quantity) {
+        if (arrayIndex(book) < 0)
+            return -1;
+        else {
+            int num = arrayIndex(book);
+            if ((Integer) array[num][1] > quantity)
+                array[num][1] = (Integer) array[num][1] - quantity;
+            return (Integer) array[num][1];
+        }
+        return -1;
+    }
+
+    int arrayIndex(Book book) {
+        for (int i = 0; i < count; i++) {
+            if (array[i][0].equals(book)) return i;
+        }
+        return -1;
+    }
+
+    int getQuantity(Book book) {
+        int num = arrayIndex(book);
+        if (num >= 0)
+            return (Integer) array[num][1];
+        return -1;
+
+    }
+}
