6e3b90b87d6f3959d9c48615f1327b95a291ad9a
Vladimir Sushchen <ya_vladimir@list.ru>
2017-11-01 13:51:18 +0300

    Лекция , задание шифрования строк улучшено

--- Lecture3/Crypt.java
+++ Lecture3/Crypt.java
@@ -1,25 +1,25 @@
 package Lecture3;
 
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class Crypt {
-     private static  byte[] key ;
-    static String strAllowedCharacters =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static byte[] key;
+
 
     public static byte[] encode(String text) {
         byte[] txt = text.getBytes();
         key = new byte[text.length()];
         byte[] res = new byte[text.length()];
 
-        key=getNextRandomString(strAllowedCharacters,text.length()).getBytes();
+        key = getNextRandomString( text.length());
         for (int i = 0; i < txt.length; i++) {
             res[i] = (byte) (txt[i] ^ key[i % key.length]);
         }
 
         return res;
     }
+
     public static String decode(byte[] pText) {
         byte[] res = new byte[pText.length];
 
@@ -29,30 +29,27 @@ public class Crypt {
 
         return new String(res);
     }
-    private static String getNextRandomString(String strAllowedCharacters,int size) {
 
-        StringBuilder sbRandomString = new StringBuilder(size);
+    private static byte[] getNextRandomString(int size) {
 
-        for(int i = 0 ; i < size; i++){
-
-            //get random integer between 0 and string length
-            Random random= new Random();
-            int randomInt = random.nextInt(strAllowedCharacters.length());
-
-            //get char from randomInt index from string and append in StringBuilder
-            sbRandomString.append( strAllowedCharacters.charAt(randomInt) );
-        }
-        return sbRandomString.toString();
+        SecureRandom rnd = new SecureRandom();
+        byte[] token = new byte[size];
+        rnd.nextBytes(token);
+        return token;
     }
 
 
     public static void main(String[] args) {
-        byte[] arr=encode("Hello world");
-        String str=new String(arr);
-        System.out.println(str);
-        String newstr= decode (arr);
-        System.out.println(newstr);
-        System.out.println(new String(key));
+        String str1="Hello world";
+        System.out.println(" String: " + str1);
+        byte[] arr = encode(str1);
+        String str = new String(arr);
+        System.out.println("key:"+ new String(key));
+        System.out.println(" String code  " + str);
+
+        String newstr = decode(arr);
+        System.out.println(" decode String " + newstr);
+
 
 
     }
